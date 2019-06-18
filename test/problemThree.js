a93a54f043464d32d02cad67befcf60c482a7d84
horngyih <horngyih@yahoo.com>
2019-06-18 12:45:15 +0800

    Completed README description of the included problems; fixed typo in problemThree description

--- test/problemThree.js
+++ test/problemThree.js
@@ -4,7 +4,7 @@ let {problem} = require("../src/problem3");
 let target = 600851475143;
 let answer = 6857;
 
-describe( "Problem 3 : Largest Prime factor\n The prime factors of 13195 are 5,7,13 and 29\n\nj What is the largest prime factor of the number 600851475143?\n",()=>{
+describe( "Problem 3 : Largest Prime factor\n The prime factors of 13195 are 5,7,13 and 29\n\n What is the largest prime factor of the number 600851475143?\n",()=>{
     it( `The answer ${answer}`, ()=>{
         assert( problem(target) === answer, `Should provide the answer : ${answer}` );
     });
